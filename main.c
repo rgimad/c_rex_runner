00a05bec4ad1036d1f7640deb5aa633cb2a77c9b
rgimad <rustem.gimadutdinov@gmail.com>
2023-12-29 01:47:50 +0300

    drawing digits in distanceMeter works

--- main.c
+++ main.c
@@ -30,13 +30,76 @@
 #define RUNNER_SPEED 6
 #define RUNNER_SPEED_DROP_COEFFICIENT 3
 
+// Distance meter configurations
+#define DM_MAX_DISTANCE_UNITS 5
+#define DM_ACHIEVEMENT_DISTANCE 100
+#define DM_COEFFICIENT 0.025
+#define DM_FLASH_DURATION 1000/4
+#define DM_FLASH_ITERATIONS 3
+#define DM_WIDTH 10
+#define DM_HEIGHT 13
+#define DM_DEST_WIDTH 11
+
+#define ATLAS_CACTUS_LARGE_X 332
+#define ATLAS_CACTUS_LARGE_Y 2
+#define ATLAS_CACTUS_SMALL_X 228
+#define ATLAS_CACTUS_SMALL_Y 2
+#define ATLAS_CLOUD_X 86
+#define ATLAS_CLOUD_Y 2
+#define ATLAS_HORIZON_X 2
+#define ATLAS_HORIZON 54
+#define ATLAS_MOON_X 484
+#define ATLAS_MOON_Y 2
+#define ATLAS_PTERODACTYL_X 134
+#define ATLAS_PTERODACTYL_Y 2
+#define ATLAS_RESTART_X 2
+#define ATLAS_RESTART_Y 2
+#define ATLAS_TEXT_SPRITE_X 655
+#define ATLAS_TEXT_SPRITE_Y 2
+#define ATLAS_TREX_X 848
+#define ATLAS_TREX_Y 2
+#define ATLAS_STAR_X 645
+#define ATLAS_STAR_Y 2
+
 SDL_Renderer *renderer;
 
-//typedef struct {
-//	//
-//} DistanceMeter;
+void blitAtlasImage(int atlasX, int atlasY, int destX, int destY, int w, int h, bool center);
 
-typedef struct {
+struct {
+	//int spritePos;s
+	int x;
+	int y;
+	int currentDistance;
+	int maxScore;
+	int highScore;
+	int digits[DM_MAX_DISTANCE_UNITS];
+	bool achievement;
+	// defaultString
+	int flashTimer;
+	int flashIterations;
+	bool invertTrigger;
+	int maxScoreUnits;
+} distanceMeter;
+
+void distanceMeterInit() {
+	distanceMeter.x = 0;
+	distanceMeter.y = 5;
+	distanceMeter.currentDistance = 0;
+	distanceMeter.maxScore = 0;
+	distanceMeter.highScore = 0;
+	distanceMeter.achievement = false;
+	distanceMeter.flashTimer = 0;
+	distanceMeter.flashIterations = 0;
+	distanceMeter.invertTrigger = false;
+	distanceMeter.maxScoreUnits = DM_MAX_DISTANCE_UNITS;
+	// init();
+}
+
+void distanceMeterDraw(int digitPos, int value/*, opt_highscore*/) {
+	blitAtlasImage(DM_WIDTH*value + ATLAS_TEXT_SPRITE_X, 0 + ATLAS_TEXT_SPRITE_Y, digitPos*DM_DEST_WIDTH, distanceMeter.y, DM_WIDTH, DM_HEIGHT, false);
+}
+
+struct {
 	int width;
 	int height;
 	// trex
@@ -61,9 +124,16 @@ typedef struct {
 	SDL_Texture* spriteAtlas;
 	// images
 	// imagesLoaded
-} Runner;
+} runner; // singletone
 
-Runner runner; // singletone
+void blitAtlasImage(int atlasX, int atlasY, int destX, int destY, int w, int h, bool center) {
+	SDL_Rect dest = { destX, destY, w, h }, src = { atlasX, atlasY, w, h };
+	if (center) {
+		dest.x -= (dest.w / 2);
+		dest.y -= (dest.h / 2);
+	}
+	SDL_RenderCopy(renderer, runner.spriteAtlas, &src, &dest);
+}
 
 void runnerLoadImages() {
 	runner.spriteAtlas = IMG_LoadTexture(renderer, "assets/sprites100.png");
@@ -94,15 +164,6 @@ void runnerInit() {
 	runnerAdjustDimensions();
 }
 
-void blitAtlasImage(int atlasX, int atlasY, int destX, int destY, int w, int h, bool center) {
-	SDL_Rect dest = { destX, destY, w, h }, src = {atlasX, atlasY, w, h};
-	if (center) {
-		dest.x -= (dest.w / 2);
-		dest.y -= (dest.h / 2);
-	}
-	SDL_RenderCopy(renderer, runner.spriteAtlas, &src, &dest);
-}
-
 int main(int argc, char* args[]) {
 	//ulist_test();
 
@@ -118,7 +179,7 @@ int main(int argc, char* args[]) {
 		return -1;
 	}
 	//Create window
-	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1300, 480, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("C Rex Runner", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1300, 480, SDL_WINDOW_SHOWN);
 	if (window == NULL) {
 		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		return -1;
@@ -127,6 +188,8 @@ int main(int argc, char* args[]) {
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	runnerInit();
+	
+	distanceMeterInit();//tmp here
 
 	//Hack to get window to stay up
 	SDL_Event e;
@@ -151,6 +214,11 @@ int main(int argc, char* args[]) {
 		*/
 		blitAtlasImage(332, 2, 50, 100, 25, 50, false);
 		blitAtlasImage(332, 2, 50, 100, 25, 50, true);
+		
+		for (int i = 0; i < 37; i++) {
+			distanceMeterDraw(i, i % 10);
+		}
+		
 		// flip the backbuffer
 		// this means that everything that we prepared behind the screens is actually shown
 		SDL_RenderPresent(renderer);
