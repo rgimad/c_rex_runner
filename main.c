4b4a3c670c9a3730765cafec6bdea8c953ddd687
rgimad <rustem.gimadutdinov@gmail.com>
2024-01-01 22:09:16 +0300

    finished distancemeter, seems to work

--- main.c
+++ main.c
@@ -5,12 +5,33 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "ulist.h"
+
+#pragma warning(disable:4996)
 
 int getRandomNumber(int _min, int _max) {
 	return rand() % (_max - _min + 1) + _min;
 }
+
+void intToStr(int num, int ndigits, char* result) {
+	char num_str[16]; // 16 more than enough for int
+	sprintf(num_str, "%d", num); // Convert num to a string
+	if (strlen(num_str) > ndigits) {
+		// Copy only the last ndigits to result
+		strcpy(result, num_str + strlen(num_str) - ndigits);
+	}
+	else {
+		// Pad the string with leading zeros until it reaches a length of ndigits
+		int z = ndigits - strlen(num_str);
+		for (int i = 0; i < z; i++) {
+			result[i] = '0';
+		}
+		strcpy(result + z, num_str);
+	}
+}
+
 
 #define DEFAULT_WIDTH 600;
 #define FPS 60
@@ -84,8 +105,8 @@ struct {
 	int y;
 	int currentDistance;
 	int maxScore;
-	int highScore;
-	int digits[DM_MAX_DISTANCE_UNITS];
+	char digits[16];
+	char highScore[16];
 	bool achievement;
 	// defaultString
 	int flashTimer;
@@ -98,29 +119,28 @@ void distanceMeterInit(int w);
 void distanceMeterCalcXPos(int w);
 void distanceMeterDraw(int digitPos, int value, bool opt_highscore);
 int distanceMeterGetActualDistance(int distance);
-// TODO:
-// update
-// drawHightScore
-// setHightScore
-// reset
+bool distanceMeterUpdate(int deltaTime, int _distance);
+void distanceMeterDrawHighScore();
+void distanceMeterSetHighScore(int _distance);
+void distanceMeterReset();
 
 void distanceMeterInit(int w) {
 	distanceMeter.x = 0;
 	distanceMeter.y = 5;
 	distanceMeter.currentDistance = 0;
 	distanceMeter.maxScore = 0;
-	distanceMeter.highScore = 0;
 	distanceMeter.achievement = false;
 	distanceMeter.flashTimer = 0;
 	distanceMeter.flashIterations = 0;
 	distanceMeter.invertTrigger = false;
 	distanceMeter.maxScoreUnits = DM_MAX_DISTANCE_UNITS;
-	// init();
 	distanceMeterCalcXPos(w);
 	for (int i = 0; i < distanceMeter.maxScoreUnits; i++) {
 		distanceMeterDraw(i, 0, false);
 	}
 	distanceMeter.maxScore = (int)pow(10, distanceMeter.maxScoreUnits) - 1;
+	distanceMeter.digits[0] = '\0';
+	distanceMeter.highScore[0] = '\0';
 }
 
 void distanceMeterCalcXPos(int w) {
@@ -128,6 +148,7 @@ void distanceMeterCalcXPos(int w) {
 }
 
 void distanceMeterDraw(int digitPos, int value, bool opt_highscore) {
+	
 	int dx, dy;
 	if (opt_highscore) {
 		dx = distanceMeter.x - (distanceMeter.maxScoreUnits * 2) * DM_WIDTH;
@@ -137,6 +158,7 @@ void distanceMeterDraw(int digitPos, int value, bool opt_highscore) {
 		dx = distanceMeter.x;
 		dy = distanceMeter.y;
 	}
+	//printf("%d %d %d  %d %d    %d\n", digitPos, value, opt_highscore, dx, dy, DM_WIDTH * value + ATLAS_TEXT_SPRITE_X);
 	blitAtlasImage(
 		DM_WIDTH*value + ATLAS_TEXT_SPRITE_X,
 		0 + ATLAS_TEXT_SPRITE_Y,
@@ -148,10 +170,90 @@ void distanceMeterDraw(int digitPos, int value, bool opt_highscore) {
 	);
 }
 
+void distanceMeterDrawHighScore() {
+	// TODO canvasCtx.globalAlpha = .8;
+	for (int i = (int)strlen(distanceMeter.highScore) - 1; i >= 0; i--) {
+		distanceMeterDraw(i, distanceMeter.highScore[i] > 12 ? distanceMeter.highScore[i] - '0' : distanceMeter.highScore[i], true);
+	}
+}
+
+void distanceMeterSetHighScore(int _distance) {
+	int distance = distanceMeterGetActualDistance(_distance);
+	distanceMeter.highScore[0] = 10;
+	distanceMeter.highScore[1] = 11;
+	distanceMeter.highScore[2] = 12;
+	intToStr(distance, distanceMeter.maxScoreUnits, distanceMeter.highScore + 3);
+}
+
+void distanceMeterReset() {
+	distanceMeterUpdate(0, 0);
+	distanceMeter.achievement = false;
+}
+
 int distanceMeterGetActualDistance(int distance) {
 	return distance ? (int)round(distance * DM_COEFFICIENT) : 0;
 }
 
+bool distanceMeterUpdate(int deltaTime, int _distance) {
+	bool paint = true;
+	bool playSound = false;
+	int distance = _distance;
+	if (!distanceMeter.achievement) {
+		distance = distanceMeterGetActualDistance(_distance);
+		// check if score has gone beyond the initial digit count.
+		if (distance > distanceMeter.maxScore && distanceMeter.maxScoreUnits == DM_MAX_DISTANCE_UNITS) {
+			distanceMeter.maxScoreUnits++;
+			distanceMeter.maxScore = distanceMeter.maxScore * 10 + 9;
+		}
+		// else {
+			// this.distance was in original but i didsnt see any usage of this field
+		// }
+
+		if (distance > 0) {
+			// Acheivement unlocked
+			if (distance % DM_ACHIEVEMENT_DISTANCE == 0) {
+				// Flash score and play sound
+				distanceMeter.achievement = true;
+				distanceMeter.flashTimer = 0;
+				playSound = true;
+			}
+			// Create a string representation of the distance with leading 0.
+			intToStr(distance, distanceMeter.maxScoreUnits, distanceMeter.digits);
+		}
+		else {
+			intToStr(0, distanceMeter.maxScoreUnits, distanceMeter.digits);
+		}
+	}
+	else {
+		// Control flashing of the score on reaching acheivement.
+		if (distanceMeter.flashIterations <= DM_FLASH_ITERATIONS) {
+			distanceMeter.flashTimer += deltaTime;
+
+			if (distanceMeter.flashTimer < DM_FLASH_DURATION) {
+				paint = false;
+			}
+			else if (distanceMeter.flashTimer > DM_FLASH_DURATION * 2) {
+				distanceMeter.flashTimer = 0;
+				distanceMeter.flashIterations++;
+			}
+		}
+		else {
+			distanceMeter.achievement = false;
+			distanceMeter.flashIterations = 0;
+			distanceMeter.flashTimer = 0;
+		}
+	}
+
+	// Draw the digits if not flashing
+	if (paint) {
+		for (int i = distanceMeter.maxScoreUnits - 1; i >= 0; i--) {
+			distanceMeterDraw(i, (int)distanceMeter.digits[i] - '0', false);
+		}
+	}
+
+	distanceMeterDrawHighScore();
+	return playSound;
+}
 
 typedef struct {
 	int width;
@@ -162,7 +264,7 @@ typedef struct {
 } Cloud;
 
 void cloudInit();
-void cloudDraw(Cloud* cloud);
+void cloudDraw(const Cloud* cloud);
 void cloudUpdate(Cloud* cloud, double speed);
 bool cloudIsVisible(const Cloud* cloud);
 
@@ -309,7 +411,7 @@ void runnerAdjustDimensions() {
 
 
 int main(int argc, char* args[]) {
-	srand(time(NULL)); // Seed the random number generator
+	srand((unsigned int)time(NULL)); // Seed the random number generator
 
 	//ulist_test();
 
@@ -342,6 +444,10 @@ int main(int argc, char* args[]) {
 	// 	c.xPos = i * 60;
 	// 	cloudDraw(&c);
 	// }
+	// 
+	// test distance meter:
+	distanceMeterSetHighScore(5656);
+	distanceMeterReset();
 
 	SDL_RenderPresent(renderer);
 
